b48c12dc266fb09a181b5ff74917f4449c1d910b
Roshan Sahani <roshan@neokred.tech>
2023-11-30 12:30:19 +0530

    fix

---
+++ payload-movies/src/payload/collections/Actors.ts
@@ -0,0 +1,45 @@
+import { CollectionConfig, FieldHook } from 'payload/types'
+
+const populateFullTitle: FieldHook = async ({ data }) => {
+  return `${data?.first_name} ${data?.last_name}`
+}
+
+const Actors: CollectionConfig = {
+  slug: 'actors',
+  admin: {
+    useAsTitle: 'full_name',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'first_name',
+      type: 'text',
+      label: 'First Name',
+    },
+    {
+      name: 'last_name',
+      type: 'text',
+      label: 'Last Name',
+    },
+    {
+      name: 'full_name',
+      type: 'text',
+      admin: {
+        components: {
+          Field: () => null,
+        },
+      },
+      hooks: {
+        beforeValidate: [populateFullTitle],
+      },
+      access: {
+        read: () => true,
+      },
+    },
+  ],
+  timestamps: false,
+}
+
+export default Actors
